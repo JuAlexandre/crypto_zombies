0b165e03cbe68ef9b922e16c9a0654c701b14b37
Julien Alexandre <contact@julienalexandre.com>
2022-02-26 16:02:03 +0100

    advanced concepts in solidity

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -1,38 +1,47 @@
 pragma solidity ^0.8.0;
 
-contract ZombieFactory {
+import "./ownable.sol";
 
-    event NewZombie(uint zombieId, string name, uint dna);
+contract ZombieFactory is Ownable {
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
 
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+    uint256 cooldownTime = 1 days;
 
     struct Zombie {
         string name;
-        uint dna;
+        uint256 dna;
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombie[] public zombies;
 
-    mapping (uint => address) public zombieToOwner;
-    mapping (address => uint) ownerZombieCount;
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
 
-    function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+    function _createZombie(string _name, uint256 _dna) internal {
+        Zombie memory zombie = Zombie(
+            _name,
+            _dna,
+            1,
+            uint32(now + cooldownTime)
+        );
+        uint256 id = zombies.push(zombie) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns (uint) {
-        uint rand = uint(keccak256(_str));
+    function _generateRandomDna(string _str) private view returns (uint256) {
+        uint256 rand = uint256(keccak256(_str));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string _name) public {
         require(ownerZombieCount[msg.sender] == 0);
-        uint randDna = _generateRandomDna(_name);
+        uint256 randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
-
 }
